d65401cb20c5764f788dc2a13ceccc9cf42b5f5f
IuryAlves <iuryalves20@gmail.com>
2021-05-04 13:03:09 +0200

    Wait for KCC pods to be ready before using it

--- create.sh
+++ create.sh
@@ -10,9 +10,17 @@ gcloud container clusters get-credentials test-cluster \
     --project $project_id \
     --zone=$region
 
-echo "Creating kubernetes resources"
+echo "Creating kubernetes resources ..."
 
-envsubst < kubernetes/pub-sub-subscription.yaml | kubectl apply -f-
-envsubst < kubernetes/pub-sub-topic.yaml | kubectl apply -f-
-envsubst < kubernetes/namespace.yaml | kubectl apply -f-
+
 envsubst < kubernetes/config-connector.yaml | kubectl apply -f-
+
+echo "Waiting for config-connector to be ready ..."
+
+kubectl wait -n cnrm-system \
+      --for=condition=Ready pod --all
+
+envsubst < kubernetes/namespace.yaml | kubectl apply -f-
+envsubst < kubernetes/enable-pubsub.yaml | kubectl apply -f-
+envsubst < kubernetes/pub-sub-topic.yaml | kubectl apply -f-
+envsubst < kubernetes/pub-sub-subscription.yaml | kubectl apply -f-
